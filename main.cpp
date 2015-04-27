8be20616374bab5173c323432c8c7d9236583acb
radek-p <radek25c@gmail.com>
2015-04-28 00:08:09 +0200

    Image comparison

--- main.cpp
+++ main.cpp
@@ -9,34 +9,76 @@
 using namespace std;
 using namespace cv;
 
-
-class Leaf {
-public:
-	Leaf(Mat image, Size sizeBeforeCrop) :
-		image(image),
-		sizeBeforeCrop(sizeBeforeCrop)
-	{ }
-
-	Mat image;
-	Size sizeBeforeCrop;
+struct ImageData {
+	Mat    img;
+	Point  massCentre;
+	string fileName;
 };
 
-Mat cropImage(Mat img) {
+float getDistance(const ImageData *d1, const ImageData *d2) {
 
-	const int height = 26; // Średnia wysokość dużej litery na zbiorze danych.
+	const int maxSizeDiff = 4;
+	if (abs(d1->img.cols - d2->img.cols) > maxSizeDiff || abs(d1->img.rows - d2->img.rows) > maxSizeDiff)
+		return 128.0;
 
+	Point tl = Point(
+		-min(d1->massCentre.x, d2->massCentre.x),
+		-min(d1->massCentre.y, d2->massCentre.y)
+	);
+	Point br = Point(
+		min(d1->img.cols - d1->massCentre.x, d2->img.cols - d2->massCentre.x),
+		min(d1->img.rows - d1->massCentre.y, d2->img.rows - d2->massCentre.y)
+	);
+
+	int sum = 0;
+	int weights = 0;
+	for (int dy = tl.y; dy <= br.y; ++dy) {
+		for (int dx = tl.x; dx <= br.x; ++dx) {
+			uchar a = d1->img.at<uchar>(d1->massCentre.y + dy, d1->massCentre.x + dx);
+			uchar b = d2->img.at<uchar>(d2->massCentre.y + dy, d2->massCentre.x + dx);
+			sum     += min(510 - a - b, 255) * abs(a - b);
+			weights += min(510 - a - b, 255);
+		}
+	}
+
+	return (float)sum / weights;
+}
+
+void preprocessImage(Mat &img) {
+// Erosion
+//	const int erosionSize = 1;
+//	static Mat element = getStructuringElement(
+//		MORPH_ELLIPSE,
+//		Size( 2 * erosionSize + 1, 2 * erosionSize + 1 ),
+//		Point( erosionSize, erosionSize )
+//	);
+
+//	erode (img, img, element);
+//	dilate(img, img, element);
+
+// Blur
+	GaussianBlur(img, img, Size(3, 3), 0);
+}
+
+void cropImage(Mat &img, Mat &res, Point &massCentre) {
+
+	massCentre = Point(0, 0);
+	int weightsSum = 0;
 	bool rowEmpty;
-
 	int fstRow = img.rows, lastRow = -1;
 	int fstCol = img.cols, lastCol = -1;
 
 	for (int y = 0; y < img.rows; ++y) {
 		rowEmpty = true;
 		for (int x = 0; x < img.cols; ++x) {
-			if (img.at<uchar>(y, x) != 255) {
+			uchar val = img.at<uchar>(y, x);
+
+			if (val != 255) {
 				fstCol   = min( fstCol, x);
 				lastCol  = max(lastCol, x);
 				rowEmpty = false;
+				massCentre += Point(x, y) * (255 - val);
+				weightsSum += (255 - val);
 			}
 		}
 
@@ -46,32 +88,12 @@ Mat cropImage(Mat img) {
 		}
 	}
 
-	// Mat res = img(Rect(fstCol, fstRow, lastCol - fstCol + 1, lastRow - fstRow + 1));
-	// cerr << "fstRow: " << fstRow << ", fstCol" << fstCol << ", lastRow: " << lastRow << ", lastCol: " << lastCol << endl;
-	// cerr << "width: " << lastCol - fstCol << endl;
-	// cerr << "height: " << lastRow - fstRow << endl;
-	//	imshow("img", res);
-	//	waitKey(0);
-
-	Mat res = 255 - Mat(height, lastCol - fstCol + 1, CV_8UC1);
-
-	int h = min(height, lastRow - fstRow + 1);
-
-//	int top = (h <= height) ?
-
-	// |    |      ***********************   |    |
-	//	res()
-
-	return res;
+	massCentre = Point((float)massCentre.x / weightsSum - fstCol, (float)massCentre.y / weightsSum - fstRow);
+	res = img(Rect(fstCol, fstRow, lastCol - fstCol + 1, lastRow - fstRow + 1));
 }
 
-shared_ptr<Leaf> createLeaf(Mat &img) {
-
-	return shared_ptr<Leaf>(new Leaf(img, Size(img.cols, img.rows)));
-}
-
-vector<shared_ptr<Leaf>> openImages() {
-	vector<shared_ptr<Leaf>> res;
+void openImages(vector<ImageData *> &images) {
+	images.clear();
 
 	int i = 0;
 	cerr << "Opening images.";
@@ -88,68 +110,46 @@ vector<shared_ptr<Leaf>> openImages() {
 		if (tmp.empty())
 			break;
 
-		res.push_back(createLeaf(tmp));
+		preprocessImage(tmp);
+
+		ImageData *data = new ImageData;
+
+		Point centre;
+		cropImage(tmp, data->img, data->massCentre);
+
+		images.push_back(data);
 	}
 
 	cerr << endl;
-
-	return res;
-}
-
-void processImage(shared_ptr<Leaf> img) {
-
-	//	cv::resize(img, img, Size(8, 13));
-
-	if (img->image.rows >= 6 && img->image.rows <= 9) {
-
-		// imshow("img", img->image);
-		// waitKey(500);
-
-		cerr << "height: " << img->sizeBeforeCrop.height << " -> " << img->image.rows << endl;
-	}
 }
 
 int main()
 {
-	namedWindow("img", CV_WINDOW_NORMAL);
 
-	vector<shared_ptr<Leaf>> images = openImages();
-	vector<int> lengths, heights;
+	vector<ImageData *> data;
+	openImages(data);
 
-	cerr << "Computing lengths";
+	cerr << "Images opened" << endl;
 
-	int lsum = 0, hsum = 0;
-	for (int i = 0; i < images.size(); ++i) {
-		if (i % 100 == 0)
-			cerr << ".";
+	for (int i = 0; i < data.size(); ++i) {
+		float minDist = 1000;
+		float dist;
+		int minIdx = -1;
+		for (int j = 0; j < data.size(); ++j) {
+			dist = getDistance(data[i], data[j]);
+			if (dist < minDist && i != j) {
+				minDist = dist;
+				minIdx = j;
+			}
+		}
 
-		while (lengths.size() <= images[i]->image.cols)
-			lengths.push_back(0);
-
-		while (heights.size() <= images[i]->image.rows)
-			heights.push_back(0);
-
-		++lengths[images[i]->image.cols];
-		++heights[images[i]->image.rows];
-
-		lsum += images[i]->image.cols;
-		hsum += images[i]->image.rows;
-
-		processImage(images[i]);
+		imshow("img_a", data[i]->img);
+		imshow("img_b", data[minIdx]->img);
+		cerr << "distance: " << minDist;
+		waitKey(0);
 	}
 
-	cerr << endl;
-
-	std::cout << "Lengths" << std::endl;
-	for (size_t i = 0; i < lengths.size(); ++i)
-		std::cout << i << "\t" << lengths[i] << std::endl;
-
-	std::cout << "Heights" << std::endl;
-	for (size_t i = 0; i < heights.size(); ++i)
-		std::cout << i << "\t" << heights[i] << std::endl;
-
-	cout << "h avg" << (float) hsum / images.size() << endl;
-	cout << "w avg" << (float) lsum / images.size() << endl;
 
 	return 0;
 }
+
